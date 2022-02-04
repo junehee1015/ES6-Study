c1904e0b03c2151e75a0547871b1717f4b4360ea
Jo Jun Hee <junehee2020@gmail.com>
2022-02-05 00:42:24 +0900

    Prototype Object (2022.02.05)

--- Prototype Object.js
+++ Prototype Object.js
@@ -8,8 +8,10 @@
 // fcuntion                                /       Prototype Object
 // Prototype 속성 -> Prototype Object 참조 /        Constructor -> function 참조
 
+
 function animal(){}
 // 생성자 함수와 new 연산자를 통해서 만들어지는 모든 Object의 Prototype(원형)이 되는 Object.
+
 
 let tiger = new animal();
 let lion = new animal();
@@ -18,10 +20,11 @@ let lion = new animal();
 
 console.log(tiger);
 console.log(lion);
-
 // animal Prototype Object는 tiger, lion의 원형이 되는 객체이다.
 // 따라서 tiger, lion은 animal Prototype Object에 접근이 가능하다.
 // animal Prototype Object에 멤버를 추가하면 tiger, lion에서도 멤버를 공유하여 사용 가능하다.
+
+
 
 // animal Prototype Object에 멤버 추가하는 방법
 // animal function에 method를 직접 만들지 않고 Prototype Object에 만드는 이유는
@@ -34,11 +37,19 @@ console.log(tiger.animal_run());
 console.log(lion.animal_run());
 
 
-
 // animal Prototype Object는 부모의 입장이고 tiger, lion은 자식의 입장이다.
-// 만약 tiger, lion에 같은 method가 있으면 그것을 실행하고 아니면 부모에서 찾아 실행한다.
+// 만약 tiger, lion에 같은 method가 있으면(재정의 하면 overriding?) 그것을 실행하고 아니면 부모에서 찾아 실행한다.
 tiger.animal_run = function() {
     return 'tiger가 달린다.'
 }
 console.log(tiger.animal_run());
 console.log(lion.animal_run());
+// 함수의 Prototype 속성으로도 접근이 가능.
+console.log(animal.prototype.animal_run());
+
+
+// 속성 추가
+tiger.leg = 4;
+console.log(tiger.leg); // 4
+console.log(lion.leg); // undefined
+// lion은 자신한테도 없고 부모(Prototype Object)에도 없으니 undefined가 출력된다.
