9900726ef85c0a9fe4bdde198f6cf08f1bec1c4f
Jo Jun Hee <junehee2020@gmail.com>
2022-02-15 23:58:12 +0900

    NodeList와 Array의 차이 (2022.02.15)

---
+++ NodeList와 Array의 차이.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// document.querySelector(); -> NodeList 객체로 반환
+// []...document.querySelector()]; -> Array 객체로 반환
+
+// NodeList
+// 1. 브라우저 API
+// 2. length method를 사용할 수 있다.
+// 3. item() method를 사용하여 nodeList.item(index) index에 따른 값을 불러올 수 있다.
+// 4. 참조에서 벗어났을 때 null
+// 5. 요소 안의 text를 볼 때 -> nodeList.item(index).innerText
+
+// Array
+// 1. javascript API
+// 2. length method를 사용할 수 있다.
+// 3. array[index]로 index에 따른 값을 불러올 수 있다.
+// 4. 참조에서 벗어났을 때 undefined
+// 5. 요소 안의 text를 볼 때 -> array[index].innerText
+
